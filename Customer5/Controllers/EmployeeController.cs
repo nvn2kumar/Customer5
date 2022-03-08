73812e88aae12771f86796efd825b44ad27ed39a
naveenk4 <naveenk4@CHETUIWK0451>
2022-03-08 23:04:44 +0530

    Add project files.

---
+++ Customer5/Controllers/EmployeeController.cs
@@ -0,0 +1,111 @@
+﻿using Employee5.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employee5.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private ApplicationDbContext dbcontext;
+        private IHostingEnvironment Environment;
+        public EmployeeController(ApplicationDbContext _dbcontext, IHostingEnvironment environment)
+        {
+            dbcontext = _dbcontext;
+            Environment = environment;
+        }
+        public IActionResult Index()
+        {
+            var emps = dbcontext.Employees.ToList();
+            
+            return View(emps);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+           
+           var files = Request.Form.Files;
+            string dbPath = string.Empty;
+            if (files.Count > 0)
+            {
+                string path = Environment.WebRootPath;
+                //string path2 = Environment.ContentRootPath;
+                string fullpath = Path.Combine(path, "images", files[0].FileName);
+
+                dbPath = "images/" + files[0].FileName;
+
+                FileStream stream = new FileStream(fullpath, FileMode.Create);
+                files[0].CopyTo(stream);
+
+            }
+            employee.Image = dbPath;
+            dbcontext.Employees.Add(employee);
+            dbcontext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+       
+
+        public IActionResult Details(int id)
+        {
+            var emps = dbcontext.Employees.Where(e => e.Id == id).FirstOrDefault();
+            return View(emps);
+        }
+
+
+        public IActionResult Delete(int id)
+        {
+            var emps = dbcontext.Employees.Where(e => e.Id == id).FirstOrDefault();
+
+            if (emps != null)
+            {
+                dbcontext.Employees.Remove(emps);
+                dbcontext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+           
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var emps = dbcontext.Employees.Where(e => e.Id == id).FirstOrDefault();
+            return View(emps);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Employee employee)
+        {
+
+            var files = Request.Form.Files;
+            string dbPath = string.Empty;
+            if (files.Count > 0)
+            {
+                string path = Environment.WebRootPath;
+                //string path2 = Environment.ContentRootPath;
+                string fullpath = Path.Combine(path, "images", files[0].FileName);
+
+                dbPath = "images/" + files[0].FileName;
+
+                FileStream stream = new FileStream(fullpath, FileMode.Create);
+                files[0].CopyTo(stream);
+
+            }
+            employee.Image = dbPath;
+            dbcontext.Employees.Update(employee);
+            dbcontext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Customer5/Migrations/20220307105703_m1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Employee5.Migrations
+{
+    public partial class m1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
